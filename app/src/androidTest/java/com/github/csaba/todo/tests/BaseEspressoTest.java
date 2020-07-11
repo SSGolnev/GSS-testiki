a3d16721b9be171b6b806233f925668a6dc5e39c
Sergei <x-sin-x@mail.ru>
2020-07-11 16:53:59 +0400

    clear BaseEspressoTest

--- app/src/androidTest/java/com/github/csaba/todo/tests/BaseEspressoTest.java
+++ app/src/androidTest/java/com/github/csaba/todo/tests/BaseEspressoTest.java
@@ -2,15 +2,12 @@ package com.github.csaba.todo.tests;
 
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
 
 import com.github.csaba.todo.MainActivity;
 
 import org.junit.After;
 import org.junit.Rule;
-import org.junit.runner.RunWith;
 
-@RunWith(AndroidJUnit4.class)
 public class BaseEspressoTest {
 
     @Rule
